ff7915d3b25ee8dc1c21df3f66eef5610a764100
Reydel Leon Machado <contact@reydelleon.me>
2015-04-23 01:21:12 -0400

    Refactored the grid system in few pages to use Foundation grid.

--- pages/information.php
+++ pages/information.php
@@ -1,44 +1,28 @@
-	<div class="container">
-		
-		<div class="twelve columns">
-		
-			<h1>Information</h1>
-		
-			<div class="six columns alpha">
-				<h2>Contact</h2>
-				
-				<p><strong>Gilles Poulin</strong> <br />
-					T&eacute;l&eacute;phone : 819 572-0880<br>
-					<a href="mailto:gil@condosw.com">gil@condosw.com</a><br></p>
-
-					<strong>Vente<br /></strong>
-					T&eacute;l&eacute;phone : 819-993-5208 <br /></p>
-
-				
-				<hr />
-				
-				<h3>&Eacute;crivez-Nous !</h3>
-				
-				<?php include("modules/formulaire.php"); ?>
-			
-			</div>
-		
-			<div class="six columns omega">
-				<h2>Promoteurs</h2>
-				
-				<p>
-					L'expertise et la r&eacute;putation des promoteurs, messieurs Jean Audet (<a href="http://www.timeshotel.ca/grandsherbrooke/fr/index.htm" rel="external">H&ocirc;tels Times</a> et Estpace Immobilier) et 
-					Gilles Poulin (Promoteur immobilier reconnu à Sherbrooke), 
-					conf&egrave;rent une valeur ajout&eacute;e &agrave; ce luxueux projet d'envergure. 
-					Leur vaste exp&eacute;rience en construction, gestion et design vous assure un investissement rentable qui fera l'envie de tous. 
-					Soyez exigeant et misez sur la meilleure adresse qui soit. Affichez votre r&eacute;ussite; vivez l'exp&eacute;rience VV.
-				</p>
-			
-			</div>
-		
-
-			
-		</div>
-	
+<div class="row">
+	<div class="large-12 columns">
+		<h1>Information</h1>
 	</div>
-	
+</div>
+<div class="row">
+    <div class="large-6 columns">
+        <h3>Contact</h3>
+        <p><strong>Gilles Poulin</strong> <br />
+            T&eacute;l&eacute;phone : 819 572-0880<br>
+            <a href="mailto:gil@condosw.com">gil@condosw.com</a><br></p>
+            <strong>Vente<br /></strong>
+            T&eacute;l&eacute;phone : 819-993-5208 <br /></p>
+        <hr />
+        <h3>&Eacute;crivez-Nous!</h3>
+        <?php include("modules/formulaire.php"); ?>
+    </div>
+    <div class="large-6 columns">
+        <h3>Promoteurs</h3>
+        <p>
+            L'expertise et la r&eacute;putation des promoteurs, messieurs Jean Audet (<a href="http://www.timeshotel.ca/grandsherbrooke/fr/index.htm" rel="external">H&ocirc;tels Times</a> et Estpace Immobilier) et
+            Gilles Poulin (Promoteur immobilier reconnu à Sherbrooke),
+            conf&egrave;rent une valeur ajout&eacute;e &agrave; ce luxueux projet d'envergure.
+            Leur vaste exp&eacute;rience en construction, gestion et design vous assure un investissement rentable qui fera l'envie de tous.
+            Soyez exigeant et misez sur la meilleure adresse qui soit. Affichez votre r&eacute;ussite; vivez l'exp&eacute;rience VV.
+        </p>
+    </div>
+</div>
--- pages/phase-3.php
+++ pages/phase-3.php
@@ -1,9 +1,7 @@
-	<div class="container">
-		
-		<div class="twelve columns">		
+	<div class="row">
+		<div class="large-12 columns">
 			<h1>Phase 3 à venir.</h1>
 			<img src="http://www.condosvv.com/assets/images/phase-2.jpg" alt="Phase 2">	
 		</div>
-	
 	</div>
 	

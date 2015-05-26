6fb82e31d5ffdbcfdb271ddb462302c87edb061e
Reydel Leon Machado <contact@reydelleon.me>
2015-05-26 15:43:43 -0400

    Updated header text.

--- modules/header.php
+++ modules/header.php
@@ -1,12 +1,14 @@
 <div class="fullwidth clearfix header-hero" id="gallerie">
-	
-	<!-- First row -->
-	<div class="row-fluid">
-		<img class="hero-logo" src="<?php echo BASE_URL; ?>assets/images/logo-hero.png" alt="W Condominiums" />
-	</div>
 
-	<div class="row-fluid">
-	<h4 class="header-hero-lead">CONDOS 3 1/2, 4 1/2, 5 1/2, STUDIOS, LOFTS &amp; PENTHOUSES, STATIONNEMENT PRIV&Eacute; ET PAVILLON D'&Eacute;T&Eacute;</h4>
-	</div>
-	
+    <!-- First row -->
+    <div class="row-fluid">
+        <img class="hero-logo" src="<?php echo BASE_URL; ?>assets/images/logo-hero.png" alt="W Condominiums"/>
+    </div>
+
+    <div class="row-fluid">
+        <h4 class="header-hero-lead">CONDOS 3 1/2, 4 1/2, 5 1/2, STUDIOS, LOFTS &amp; PENTHOUSES, AVEC PAVILLION D’&Eacute;T&Eacute;, PARC PRIV&Eacute;, PISCINE ET PAVILLION D’&Eacute;T&Eacute;</h4>
+        <br/>
+        <h4 class="header-hero-lead">S&Eacute;CURIT&Eacute;, ASCENSEUR… GARAGE INTERI&Eacute;UR DISPONIBLE</h4>
+    </div>
+
 </div>

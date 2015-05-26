e6968481dd7f9815136b60d93153b1d9622531ce
Reydel Leon Machado <contact@reydelleon.me>
2015-05-26 18:27:36 -0400

    Fixes date in red section and changes text in header.

--- modules/header.php
+++ modules/header.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="row-fluid">
-        <h4 class="header-hero-lead">CONDOS 3 1/2, 4 1/2, 5 1/2, STUDIOS, LOFTS &amp; PENTHOUSES, AVEC PAVILLION D’&Eacute;T&Eacute;, PARC PRIV&Eacute;, PISCINE ET PAVILLION D’&Eacute;T&Eacute;</h4>
+        <h4 class="header-hero-lead">CONDOS 3 1/2, 4 1/2, 5 1/2, STUDIOS, LOFTS &amp; PENTHOUSES, AVEC PAVILLION D’&Eacute;T&Eacute;, PARC PRIV&Eacute; ET PISCINE</h4>
         <br/>
         <h4 class="header-hero-lead">S&Eacute;CURIT&Eacute;, ASCENSEUR… GARAGE INTERI&Eacute;UR DISPONIBLE</h4>
     </div>
--- pages/accueil.php
+++ pages/accueil.php
@@ -34,7 +34,7 @@
                 </div>
                 <div class="large-12 text-center columns no-space">
                     <h4>Phase 1: Liquidation démonstrateur, offrez vous le meilleur au meilleur prix! Meilleure offre acceptée.</h4>
-                    <h4>Phase 2: Profitez de notre liste de prix pré-construction avant le 25 mai 2015.</h4></div>
+                    <h4>Phase 2: Profitez de notre liste de prix pré-construction avant le 1 juin 2015.</h4></div>
                 <div class="large-4 large-offset-8 columns">
                     <h3><small>Prenez rendez-vous </small><a href="tel:+18195720880">(819) 572-0880</a></strong></h3>
                 </div>

14f77cf8dae28b1f0a9e25bea4478292af11e7e2
Reydel Leon Machado <contact@reydelleon.me>
2016-02-04 11:40:20 -0500

    Changes Mot de Promoteur text to remove some portions of it.

--- pages/information.php
+++ pages/information.php
@@ -37,7 +37,6 @@
 
         <blockquote>
             <span><b>Bienvenue,</b></span>
-            <p>C’est avec fierté qu’aujourd’hui nous amorçons la deuxième phase des Condos W.</p>
             <p>Aussi, nous sommes heureux de constater la réponse positive des acheteurs de nos condominiums pour la beauté, l'excellence et la qualité de vie urbaine exceptionnelle de notre projet.</p>
             <p>Dirigé et conçu par deux passionnés de l’immobilier et travaillant en étroite collaboration avec M. Sylvain Pomerleau, architecte, nous sommes particulièrement fiers de contribuer au développement de la municipalité de Sherbrooke de façon significative et durable.</p>
             <p>Notre clientèle apprécie pleinement  la possibilité de bénéficier d’un projet de vie urbain totalement personnalisé et unique.</p>
@@ -46,7 +45,7 @@
             <p>Avec une architecture moderne et adaptée à l’avenir, nous proposons une fenestration abondante, des balcons bord de mer surdimensionnés au premier plan de la qualité de vie qu’impose la localisation unique et plus qu’avantageuse du W.</p>
             <p>De plus, nous sommes soucieux d’offrir une qualité supérieure en conception et réalisation. Nous concevons des <strong>STUDIOS, LOFTS, CONDOMINIUMS 3 ½, 4 ½, 5 ½ & PENTHOUSE</strong> avec un souci d'excellence et sommes convaincus que la valeur ajoutée de l’unicité de votre projet, saura garantir votre investissement et ce, en créant de toutes pièces de petits chefs-d'œuvre.</p>
             <p>En achetant un condo, en plus de la qualité, la sécurité et le confort, vous bénéficierez d’une vue spectaculaire de la ville de Sherbrooke.</p>
-            <p>Aujourd’hui, parmi les condominiums disponibles immédiatement de la <strong>PHASE 1</strong>, ainsi que ceux de la <strong>PHASE 2</strong> livrables au cours de l’année 2015, nous vous offrons un vaste éventail qui saura certainement vous plaire.</p>
+            <p>Aujourd’hui, parmi les condominiums disponibles immédiatement de la <strong>PHASE 1</strong>, ainsi que ceux de la <strong>PHASE 2</strong>, nous vous offrons un vaste éventail qui saura certainement vous plaire.</p>
             <p>Faites-nous part de vos besoins et complétez notre cahier visiteur. Il nous sera probablement possible d’intégrer votre projet dans l’une de nos phases.</p>
             <p>Merci de votre visite <strong style="color: #9e0c0f">W CONDOMINIUMS VUE ET VERTIGE</strong></p>
             <cite>Gil Poulin & Jean Audet. Promoteurs</cite>
